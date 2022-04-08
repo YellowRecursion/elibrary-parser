88732178c06fca14996539967638eb291303dd38
Vadim <zinnatullin.vadim2001@yandex.ru>
2022-04-09 00:07:29 +0500

    Добавьте файлы проекта.

---
+++ eLIBRARYparsing/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace eLIBRARYparsing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(379, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 207);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(379, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(379, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select file with names";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(379, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select folder for results";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 509);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(378, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Excel файл";
+            this.openFileDialog1.Filter = "Файлы Excel|*.xlsx*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.FolderBrowserDialog1_HelpRequest);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleDescription = "";
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(405, 9);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(582, 545);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(201, 323);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(187, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(201, 353);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(187, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.UseSystemPasswordChar = true;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(378, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Elibrary login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Elibrary password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 568);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1014, 615);
+            this.MinimumSize = new System.Drawing.Size(1014, 615);
+            this.Name = "Form1";
+            this.Text = "eLibraryParser";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ eLIBRARYparsing/Form1.cs
@@ -0,0 +1,442 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+// Библиотеки для работы с excel
+using Excel = Microsoft.Office.Interop.Excel;
+using Keys = OpenQA.Selenium.Keys;
+
+namespace eLIBRARYparsing
+{
+    public partial class Form1 : Form
+    {
+        string fileDirectory;
+        string resultDirectory;
+
+        bool started;
+        Thread t;
+
+        int k = 0;
+
+        // Создаём экземпляр нашего приложения
+        Excel.Application excelApp = new Excel.Application();
+        // Создаём экземпляр рабочий книги Excel
+        Excel.Workbook workBook;
+        // Создаём экземпляр листа Excel
+        Excel.Worksheet workSheet;
+
+        string name = "Коледин Сергей Николаевич";
+
+        //StreamWriter sw;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            resultDirectory = folderBrowserDialog1.SelectedPath;
+            textBox2.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            fileDirectory = openFileDialog1.FileName;
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void FolderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Text = "Stop";
+                t = new Thread(Main);
+                t.SetApartmentState(ApartmentState.STA);
+                t.IsBackground = true;
+                t.Start();
+                print("Starting...");
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Text = "Start";
+                if (t.IsAlive)
+                {
+                    t.Abort();
+                    t.Interrupt();
+                }
+                print("Stoping...");
+                workBook.Close();
+                //if (sw != null) sw.Close();
+            }
+
+            started = !started;
+        }
+
+
+
+        void Main()
+        {
+            //try
+            {
+                Random r = new Random();
+
+                workBook = excelApp.Workbooks.Open(fileDirectory);
+                workSheet = (Excel.Worksheet)workBook.Worksheets.get_Item(1);
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+                //chromeOptions.AddArgument("headless");
+                IWebDriver driver = new ChromeDriver(chromeOptions);
+
+                //FirefoxOptions chromeOptions = new FirefoxOptions();
+                //IWebDriver driver = new FirefoxDriver(chromeOptions);
+
+                Thread.Sleep(1000);
+                driver.Url = @"https://www.elibrary.ru/";
+                IWebElement element;
+
+                //element = driver.FindElement(By.XPath(@".//input[@id='login']"));
+                //element.SendKeys(textBox3.Text);
+
+                //element = driver.FindElement(By.XPath(@".//input[@id='password']"));
+                //element.SendKeys(textBox4.Text);
+
+                element = driver.FindElement(By.XPath(@".//input[@id='login']"));
+                //element.SendKeys("vadik00056");
+                element.SendKeys(textBox3.Text);
+
+                element = driver.FindElement(By.XPath(@".//input[@id='password']"));
+                //element.SendKeys("vadim1232684");
+                element.SendKeys(textBox4.Text);
+
+                element = driver.FindElement(By.XPath(@".//div[@onclick='check_all()']"));
+                element.Click();
+
+                Thread.Sleep(3000);
+
+                while (workSheet.Cells[k + 1, 1].Text.ToString() != "")
+                {
+                    //try
+                    //{
+                    name = workSheet.Cells[k + 1, 1].Text.ToString();
+                    print((k + 1) + ". " + name);
+
+                    if (!Directory.Exists(resultDirectory + @"\Text")) Directory.CreateDirectory(resultDirectory + @"\Text");
+                    if (File.Exists(resultDirectory + @"\Text\" + name + ".txt"))
+                    {
+                        if (File.ReadAllText(resultDirectory + @"\Text\" + name + ".txt").Length > 10)
+                        {
+                            print("Информация уже имеется");
+                            k++;
+                            continue;
+                        }
+                    }
+
+                    driver.Url = @"https://www.elibrary.ru/authors.asp";
+                    Thread.Sleep(r.Next(2000, 7000));
+
+                    element = driver.FindElement(By.XPath(@".//input[@id='surname']"));
+                    element.Clear();
+                    element.SendKeys(name);
+
+                    element = driver.FindElement(By.XPath(@".//select[@name='town']"));
+                    element.SendKeys("Уфа");
+
+                    IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+                    js.ExecuteScript("author_search()");
+                    //element = driver.FindElement(By.XPath(@".//div[@onclick='author_search()']"));
+                    //Thread.Sleep(500);
+                    //element.Click();
+                    //Thread.Sleep(500);
+                    //element.Click();
+
+                    Thread.Sleep(500);
+
+                    IWebElement man;
+                    try
+                    {
+                        man = driver.FindElement(By.XPath(@".//tr[@bgcolor='#f5f5f5']"));
+                    }
+                    catch (Exception)
+                    {
+                        k++;
+                        print("Не найдено авторов, удовлетворяющих условиям поиска");
+                        continue;
+                    }
+
+
+                    Thread.Sleep(r.Next(100, 300));
+
+                    int countOfArticles;
+                    try
+                    {
+                        countOfArticles = Convert.ToInt32(man.FindElement(By.XPath(@".//a[@title='Список публикаций данного автора в РИНЦ']")).Text);
+                        var elem = man.FindElement(By.XPath(@".//a[@title='Список публикаций данного автора в РИНЦ']"));
+                        IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+                        executor.ExecuteScript("arguments[0].click();", elem);
+                    }
+                    catch (Exception)
+                    {
+                        k++;
+                        print("Не найдена кнопка 'Список публикаций данного автора в РИНЦ'");
+                        continue;
+                    }
+
+                    Thread.Sleep(1000);
+                    Thread.Sleep(r.Next(100, 300));
+
+
+                    IWebElement[] pages = driver.FindElements(By.XPath(@".//tr[@class='menurb']/td[@class='mouse-hovergr']")).ToArray();
+
+
+                    Thread.Sleep(r.Next(100, 300));
+
+                    for (int j = (countOfArticles < 21) ? -3 : 0; j < pages.Length - 2; j++) // ИСПРАВИТЬ БАГ ЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁ
+                    {
+                        print("PAGE #" + (j + 1) + "/" + (pages.Length - 2));
+                        Thread.Sleep(r.Next(3000, 6000));
+                        IWebElement[] articles = driver.FindElement(By.XPath(@".//table[@id='restab']")).FindElements(By.TagName("tr")).ToArray();
+                        if (articles.Length > 0)
+                        {
+                            for (int i = 3; i < articles.Length; i++)
+                            {
+                                print("Article: " + (i - 2));
+                                Thread.Sleep(r.Next(1000, 3000));
+
+                                articles[i] = articles[i].FindElements(By.TagName("td"))[1].FindElement(By.TagName("a"));
+
+                                // устанавливаем таймаут ожидания загрузки
+                                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+                                articles[i].SendKeys(Keys.Control + Keys.Return);
+                                Thread.Sleep(1000);
+                                try
+                                {
+                                    driver.SwitchTo().Window(driver.WindowHandles[1]);
+                                    Thread.Sleep(r.Next(100, 300));
+                                }
+                                catch (TimeoutException)
+                                {
+                                    Thread.Sleep(2000);
+                                }
+
+
+                                Thread.Sleep(200);
+                                Thread.Sleep(r.Next(100, 300));
+
+
+                                // Copy info
+
+                                IWebElement content;
+                                IWebElement[] contents;
+
+                                // Create directory, open files
+                                if (!Directory.Exists(resultDirectory + @"\Text")) Directory.CreateDirectory(resultDirectory + @"\Text");
+
+                                FileStream aFile = new FileStream(resultDirectory + @"\Text\" + name + ".txt", FileMode.OpenOrCreate);
+                                StreamWriter sw = new StreamWriter(aFile);
+                                aFile.Seek(0, SeekOrigin.End);
+
+                                if (!Directory.Exists(resultDirectory + @"\UDC")) Directory.CreateDirectory(resultDirectory + @"\UDC");
+
+                                FileStream bFile = new FileStream(resultDirectory + @"\UDC\" + name + ".txt", FileMode.OpenOrCreate);
+                                StreamWriter sw2 = new StreamWriter(bFile);
+                                bFile.Seek(0, SeekOrigin.End);
+
+                                int c = 0;
+                                while (!HasElement(driver, By.ClassName("bigtext")))
+                                {
+                                    c++;
+                                    if (c > 8) break;
+                                    Thread.Sleep(1000);
+                                }
+
+                                if (!HasElement(driver, By.ClassName("bigtext")))
+                                {
+                                    sw.Close();
+                                    sw2.Close();
+                                    print("bigtext not found");
+
+                                    int h = 0;
+                                    while (h < 10)
+                                    {
+                                        try
+                                        {
+                                            driver.Close();
+                                            break;
+                                        }
+                                        catch (WebDriverException)
+                                        {
+                                            h++;
+                                            Thread.Sleep(500);
+                                            continue;
+                                        }
+                                    }
+
+
+                                    Thread.Sleep(100);
+                                    driver.SwitchTo().Window(driver.WindowHandles[0]);
+                                    Thread.Sleep(200);
+                                    continue;
+                                }
+
+                                content = driver.FindElement(By.ClassName("bigtext"));
+
+                                // Text info copy
+                                sw.WriteLine(content.Text);
+                                contents = driver.FindElements(By.XPath(@".//table[@width='550']")).ToArray();
+                                for (int o = 0; o < contents.Length; o++)
+                                {
+                                    if (contents[o].FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"))[0].Text.Contains("КЛЮЧЕВЫЕ СЛОВА:"))
+                                    {
+                                        sw.WriteLine(contents[o].FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"))[1].Text);
+                                    }
+                                    else
+                                    if (contents[o].FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"))[0].Text.Contains("АННОТАЦИЯ:"))
+                                    {
+                                        sw.WriteLine(contents[o].FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"))[1].Text);
+                                    }
+                                    else
+                                    if (contents[o].FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"))[0].Text.Contains("КЛЮЧЕВЫЕ СЛОВА:"))
+                                    {
+                                        sw.WriteLine(contents[o].FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"))[1].Text);
+                                    }
+                                }
+                                // UDC copy
+                                contents = driver.FindElements(By.XPath(@".//td[@width='574']")).ToArray();
+                                for (int o = 0; o < contents.Length; o++)
+                                {
+                                    if (contents[o].Text.Contains("УДК:"))
+                                    {
+                                        sw2.WriteLine(contents[o].FindElement(By.TagName("font")).Text);
+                                    }
+                                }
+
+                                sw.Close();
+                                sw2.Close();
+
+                                print("OK");
+
+
+                                driver.Close();
+                                Thread.Sleep(100);
+                                driver.SwitchTo().Window(driver.WindowHandles[0]);
+                                Thread.Sleep(200);
+                            }
+                        }
+                        else
+                        {
+                            print("У автора нет статей");
+                            Thread.Sleep(2000);
+                        }
+
+                        if (j < pages.Length - 1 - 2)
+                        {
+                            try
+                            {
+                                js.ExecuteScript("goto_page(" + (j + 2) + ")");
+                            }
+                            catch (Exception)
+                            {
+                                k++;
+                                continue;
+                            }
+
+                        }
+                        Thread.Sleep(1000);
+                        Thread.Sleep(r.Next(100, 300));
+                    }
+
+                    k++;
+                    //}
+                    //catch
+                    //{
+                    //    k++;
+                    //    continue;
+                    //}
+                    //sw.Close();
+                }
+
+
+
+
+                print("Completed!");
+
+                workBook.Close();
+                //button1.Enabled = true;
+                //button2.Enabled = true;
+                //button3.Text = "Запустить";
+            }
+            //catch
+            {
+                /*MessageBox.Show("Парсер экстренно завершил работу. Возможно сайт заблокировал доступ.");
+                print("=COMPLETED=ERROR=");
+                button1.Enabled = true; // ИСПРАВИТЬТ
+                button2.Enabled = true;
+                button3.Text = "Start";
+                workBook.Close();*/
+            }
+        }
+
+        public void print(object obj)
+        {
+            richTextBox1.Invoke(new Action(() => richTextBox1.Text = richTextBox1.Text + obj.ToString() + Environment.NewLine));
+            richTextBox1.Invoke(new Action(() => richTextBox1.SelectionStart = richTextBox1.Text.Length));
+            richTextBox1.Invoke(new Action(() => richTextBox1.ScrollToCaret()));
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (workBook != null)
+                workBook.Close();
+        }
+
+        public bool HasElement(IWebDriver drv, By selector)
+        {
+            try
+            {
+                return drv.FindElements(selector).Count > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int showWindowCommand);
+    }
+}
